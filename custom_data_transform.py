89488693694d6394ec34032435882f2c39d02ea3
STELZER-AF <stelzer.af@gmail.com>
2023-07-04 12:12:27 -0300

    added filtered data

--- custom_data_transform.py
+++ custom_data_transform.py
@@ -18,28 +18,25 @@ print("File content:", file_content)
 # Parse the JSON content
 json_data = json.loads(file_content)
 
-grouped_data = {}
-for obj in json_data:
-    entity_id = obj['entity_id']
-    if entity_id not in grouped_data:
-        grouped_data[entity_id] = []
-    grouped_data[entity_id].append(obj)
 
-print("Grouped data:", grouped_data)
 
-# Process each data object
-# for data in json_data:
-#     entity_id = data['entity_id']
-#     file_name = f"{entity_id}.json"
-#     print("Entity ID:", entity_id)
+Process each data object
+for data in json_data:
+    entity_id = data['entity_id']
+    file_name = f"{entity_id}.json"
+    print("Entity ID:", entity_id)
+    
+    filtered_data = [obj for obj in data if obj.get('entity_id') == entity_id]
 
-#     # Convert the data to JSON string
-#     json_str = json.dumps(data)
+    print("Filtered data:", filtered_data)
 
-#     # Write the data to a file
-#     with open(file_name, 'w') as file:
-#         file.write(json_str)
+    # # Convert the data to JSON string
+    # json_str = json.dumps(data)
 
-#     # Upload the file to S3
-#     s3_client.upload_file(file_name, s3_bucket, file_name)
+    # # Write the data to a file
+    # with open(file_name, 'w') as file:
+    #     file.write(json_str)
+
+    # # Upload the file to S3
+    # s3_client.upload_file(file_name, s3_bucket, file_name)
 
