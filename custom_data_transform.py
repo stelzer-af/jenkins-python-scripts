5fd2fa0400bb60f6f365721f91d96226d37acf7b
STELZER-AF <stelzer.af@gmail.com>
2023-07-04 12:39:34 -0300

    fix logic

--- custom_data_transform.py
+++ custom_data_transform.py
@@ -27,24 +27,56 @@ for obj in json_data:
     grouped_data[entity_id].append(obj)
 
 # Create separate JSON arrays
-json_arrays = []
+# json_arrays = []
 for entity_id, objects in grouped_data.items():
     json_array = json.dumps(objects)
-    json_arrays.append(json_array)
 
-# Print the JSON arrays
-for json_array in json_arrays:
-    print(json_array)
-    print()
+    file_name = f"{entity_id}.json"
     
-    # Convert the data to JSON string
-    json_str = json.dumps(data)
-
-    # Write the data to a file
+     # Write the data to a file
     with open(file_name, 'w') as file:
-        file.write(json_str)
+        file.write(json_array)
 
     # Upload the file to S3
     s3_client.upload_file(file_name, s3_bucket, file_name)
+    
+    # json_arrays.append(json_array)
 
+# # Print the JSON arrays
+# for json_array in json_arrays:
+#     print(json_array)
+#     print()
+#     # Convert the data to JSON string
+#     json_str = json.dumps(json_array)
+    
+#     entity_id = data['entity_id']
+#     file_name = f"{entity_id}.json"
+
+#     # Write the data to a file
+#     with open(file_name, 'w') as file:
+#         file.write(json_str)
+
+#     # Upload the file to S3
+#     s3_client.upload_file(file_name, s3_bucket, file_name)
+
+
+# # Process each data object
+# for data in json_data:
+#     entity_id = data['entity_id']
+#     file_name = f"{entity_id}.json"
+#     print("Entity ID:", entity_id)
+    
+#     filtered_data = [obj for obj in data if obj['entity_id'] == entity_id]
+
+#     print("Filtered data:", filtered_data)
+
+    # # Convert the data to JSON string
+    # json_str = json.dumps(data)
+
+    # # Write the data to a file
+    # with open(file_name, 'w') as file:
+    #     file.write(json_str)
+
+    # # Upload the file to S3
+    # s3_client.upload_file(file_name, s3_bucket, file_name)
 
